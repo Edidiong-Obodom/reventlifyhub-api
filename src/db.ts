31ee6b786d6994932ea9b44c5912db829f08c279
Edidiong Obodom <97753291+DinnoCreator@users.noreply.github.com>
2024-03-31 16:20:09 +0100

    fix: type in package.json to module

--- src/db.ts
+++ src/db.ts
@@ -6,6 +6,7 @@ dotenv.config();
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   port: 5432,
   database: process.env.DB,
   // ssl: true,
