c852b44261ee39cbb85aaf1b865df813b5dce6a6
Edidiong Obodom <97753291+DinnoCreator@users.noreply.github.com>
2024-04-01 11:36:36 +0100

    fix: nodemailer issue

--- src/helpers/credentialsTemplate.ts
+++ src/helpers/credentialsTemplate.ts
@@ -1,6 +1,14 @@
 export const mailCredentials = {
+  service: "Outlook365",
   host: "smtp.office365.com",
+  secure: false, // true for 465, false for other ports
+  // logger: true,
+  // debug: true,
   port: 578,
+  tls: {
+    ciphers: "SSLv3",
+    rejectUnauthorized: false,
+  },
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL,
