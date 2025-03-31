c8ac81adb45e3f922d2a604d92b3c91f26efbc87
Edidiong Obodom <edijay17@gmail.com>
2025-03-31 10:53:19 +0100

    chore: modify rate limit :zap:

--- src/utilities/rate-limit/all-rate-limit/index.ts
+++ src/utilities/rate-limit/all-rate-limit/index.ts
@@ -1,11 +1,11 @@
 import rateLimit from "express-rate-limit";
 
 export const allRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 8, // Limit each IP to 8 requests per `window` (here, per 15 minutes)
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20, // Limit each IP to 20 requests per `window` (here, per 1 minutes)
   message: {
     message:
-      "Too many requests from this IP, please try again after 15 minutes before you can make this particular request.",
+      "Too many requests from this IP, please try again after 1 minute before you can make this particular request.",
   },
   headers: true, // Send rate limit info in the `RateLimit-*` headers
 });
--- src/utilities/rate-limit/login-rate-limiter/index.ts
+++ src/utilities/rate-limit/login-rate-limiter/index.ts
@@ -1,11 +1,11 @@
 import rateLimit from "express-rate-limit";
 
 export const loginRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 login requests per `window` (here, per 15 minutes)
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 10, // Limit each IP to 10 login requests per `window` (here, per 1 minutes)
   message: {
     message:
-      "Too many login attempts from this IP, please try again after 15 minutes.",
+      "Too many login attempts from this IP, please try again after 1 minute.",
   },
   headers: true, // Send rate limit info in the `RateLimit-*` headers
 });
